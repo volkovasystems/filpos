2580d8cdfdce6bbec006f6b456685d0d298f3cc0
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-10 17:58:21 +0800

    Updated test.

--- filpos-test.js
+++ filpos-test.js
@@ -3,8 +3,8 @@ const assert = require( "assert" );
 const filpos = require( "./filpos.js" );
 
 let array = [ 1, 2 ];
-assert.equal( filpos( array, 5, 0 ), array, "should be equal" );
+assert.equal( filpos( array, 5, 0 ), array, "should be equal to [ 1, 2 ]" );
 
-assert.deepEqual( filpos( array, 5, 0 ), [ 1, 2, 0, 0, 0 ], "should be deeply equal" );
+assert.deepEqual( filpos( array, 5, 0 ), [ 1, 2, 0, 0, 0 ], "should be equal to [ 1, 2, 0, 0, 0 ]" );
 
 console.log( "ok" );
